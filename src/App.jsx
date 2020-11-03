d7b824337e084aa1c0a6739adab20a0ba24eb182
Sharoness <sharong1234@hotmail.com>
2020-11-03 14:49:09 +0000

    App.js is now App.jsx; made base of portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div />
+    <div>
+      <div className="header">
+        <h1>Portfolio</h1>
+        <h2>Sharon Grashuis</h2>
+        <p>Introduce myself</p>
+        <p>To see the projects in full size, click on the name of the project under the phone.</p>
+        <div className="projects">
+          projecten
+        </div>
+      </div>
+    </div>
   );
 }
 
