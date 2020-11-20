def7c4bc3f89cb51af8859cec12bab0080555323
Sharoness <sharong1234@hotmail.com>
2020-11-20 11:48:32 +0000

    About.jsx: changed text a bit, made lay-out better

--- src/About.jsx
+++ src/About.jsx
@@ -6,16 +6,17 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 const useStyles = makeStyles({
     item: {
         flexBasis: "auto",
-        marginBottom: "30px",
+        marginBottom: "50px",
         display: "flex",
         flexDirection: "row",
         flexWrap: "wrap",
         justifyContent: "space-evenly",
         alignItems: "flex-start",
-        alignContent: "space-evenly"
+        alignContent: "space-evenly",
     },
     profile: { // flexbox item card
         maxWidth: "300px",
+        marginBottom: "30px"
     },
     media: { // avatar
         width: "300px",
@@ -25,6 +26,10 @@ const useStyles = makeStyles({
         maxWidth: "800px",
         itemAlign: "center",
         flexGrow: "2",
+        paddingTop: "10px",
+        paddingLeft: "10px",
+    },
+    link: {
     },
     root: { // github icon
         color: "black",
@@ -65,14 +70,14 @@ function About() {
             </Card>
         </Paper>
         <Typography paragraph className={classes.text}>
-            Hello! My name is Sharon, I am a Dutch Front End Web Developer living in London, UK.<br />
+            Hello! My name is Sharon, I am a Dutch Front End Web Developer living in London.<br />
 
             Since July 2019 I have been learning web development via FreeCodeCamp, under guidance of an experienced full-stack web developer.<br />
-            Next to the common technologies I learned via FreeCodeCamp, such as React, JavaScript, HTML and CSS, I have learned technologies such as HTTP, Git, Node, Package Managers, Web Bundlers and Docker.<br />
-            I have obtained the FreeCodeCamp certificates for <a href="https://www.freecodecamp.org/certification/sharoness/responsive-web-design" target="_blank" rel="noopener noreferrer">Responsive Web Design</a>, <a href="https://www.freecodecamp.org/certification/sharoness/javascript-algorithms-and-data-structures" target="_blank" rel="noopener noreferrer">JavaScript Algorithms and Data Structures</a>, and <a href="https://www.freecodecamp.org/certification/sharoness/front-end-libraries" target="_blank" rel="noopener noreferrer">Front End Libraries</a>.<br />
-            In order to obtain these certificates, I had to make projects and pass their unit tests. To see that I passed them, click on the hamburger menu, select the right name in the Test Suite and press Run Tests.<br />
-            The ones that I am proud of the most, are shown below. They are all made with <b>React</b>.<br />
-            To see a project in full screen, click on its title below. To see more of my projects, click on the GitHub icon next to my name.
+            The technologies I have learned through FreeCodeCamp and by myself, are <b>React</b>, <b>JavaScript</b>, <b>HTML</b>, <b>CSS</b>, <b>HTTP</b>, <b>Git</b>, <b>Node</b>, <b>Package Managers</b>, <b>Web Bundlers</b> and <b>Docker</b>.<br />
+            I have obtained the FreeCodeCamp certificates for <a href="https://www.freecodecamp.org/certification/sharoness/responsive-web-design" target="_blank" rel="noopener noreferrer" className={classes.link}>Responsive Web Design</a>, <a href="https://www.freecodecamp.org/certification/sharoness/javascript-algorithms-and-data-structures" target="_blank" rel="noopener noreferrer" className={classes.link}>JavaScript Algorithms and Data Structures</a>, and <a href="https://www.freecodecamp.org/certification/sharoness/front-end-libraries" target="_blank" rel="noopener noreferrer" className={classes.link}>Front End Libraries</a>.<br />
+            In order to obtain these certificates, I had to make projects and pass their unit tests.<br />
+            The ones that I'm most proud of, are shown below. They are all made with <b>React</b>.<br />
+            To see that I passed the unit tests, click on the hamburger menu, select the right name in the Test Suite and press Run Tests. To see a project in full screen, click on its title below. To see more of my projects, click on the GitHub icon next to my name.
         </Typography>
     </div>
   );
