a8ef8ed7675fb7c9221d430a10f8c1600e41e9f6
Sharoness <sharong1234@hotmail.com>
2020-11-20 18:11:40 +0000

    Updates

---
+++ precache-manifest.e320c98e8a0a44b5e8806cdda5717aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212aa9f0ace2cbb498e35808bb940bd7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "493cb99d7cdfaca1cfad",
+    "url": "/Portfolio/static/css/main.b0b49a4d.chunk.css"
+  },
+  {
+    "revision": "f01f56e47c2150336915",
+    "url": "/Portfolio/static/js/2.41e5cba3.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/Portfolio/static/js/2.41e5cba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493cb99d7cdfaca1cfad",
+    "url": "/Portfolio/static/js/main.181aee23.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
